0ebf9cd1cd72937cb5dc22cf36396452b676b0bd
Laxman Rumba <rumbavai767@gmail.com>
2025-11-26 17:45:25 +0545

    refact/auth/token: removed unnecessary codes

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 import cookie from "cookie";
-import { get } from "http";
 import { getAxiosErrorMessage } from "@/app/lib/helpers/axios.error";
 
 export default async function handler(
