f45a77a9b5a7af85f32ee42a81b168f3434319f7
Laxman Rumba <rumbavai767@gmail.com>
2025-11-29 23:55:18 +0545

    fact/createForm/ui: designed create url form ui with modal

--- app/(dashboard)/urls/page.tsx
+++ app/(dashboard)/urls/page.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import UrlTable from './components/UrlTable'
-import { Button } from '@/app/components/ui/Button'
-import { PlusIcon } from '@heroicons/react/24/outline'
+"use client"
+import Modal from '@/app/components/ui/Modal';
+import CreateUrlForm from './components/CreateUrlForm';
+import { useState } from 'react';
+import { Button } from '@/app/components/ui/Button';
+import { PlusIcon } from '@heroicons/react/24/outline';
+import UrlTable from './components/UrlTable';
 
 export default function UrlPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div>
       <div className="flex justify-between items-center mx-4 mb-4">
         <h1 className="text-2xl font-bold text-gray-900 dark:text-white">Your Shortened URLs</h1>
         <Button
-          className="flex items-center gap-2! px-4! bg-blue-600 text-white rounded-lg hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 transition hover:scale-105"
+          className="flex items-center gap-2 px-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 transition hover:scale-105"
+          onClick={() => setIsModalOpen(true)}
         >
           <PlusIcon className="h-5 w-5" />
           Add New URL
         </Button>
       </div>
-      <UrlTable />
-    </div>
-  )
-}
 
+      <UrlTable />
+
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)} title="Create New URL">
+          <CreateUrlForm  />
+        </Modal>
+      )}
+    </div>
+  );
+}
---
+++ app/components/ui/Modal.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+
+interface ModalProps {
+  onClose: () => void;
+  title?: string;
+  children: ReactNode;
+}
+
+export default function Modal({ onClose, title, children }: ModalProps) {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-sm">
+      <div className="bg-white dark:bg-gray-900 rounded-xl shadow-lg w-full max-w-lg p-6 relative">
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 dark:hover:text-gray-300 font-bold cursor-pointer"
+        >
+          ✕
+        </button>
+
+        {title && <h1 className="text-xl font-semibold text-gray-900 dark:text-white mb-4">{title}</h1>}
+
+        <div className='flex items-center justify-center'>{children}</div>
+      </div>
+    </div>
+  );
+}
