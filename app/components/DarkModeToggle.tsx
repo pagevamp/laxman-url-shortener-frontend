797a8de59ddad03951ca8ce4458461a0d55935ee
Laxman Rumba <rumbavai767@gmail.com>
2025-12-03 17:36:28 +0545

    refact/navbar: changed the navbar toggle light icon to sun

--- app/components/DarkModeToggle.tsx
+++ app/components/DarkModeToggle.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { LightBulbIcon, MoonIcon } from "@heroicons/react/24/outline";
+import { LightBulbIcon, MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 import useDarkMode from "../hooks/useDarkMode";
 import { Button } from "./Button";
@@ -13,7 +13,7 @@ export default function DarkModeToggle() {
       className="px-1!  text-gray-300! rounded-full! dark:text-gray-900! dark:bg-gray-100 transition-colors dark:hover:bg-white hover:scale-105"
     >
       {isDark ? (
-        <LightBulbIcon className="font-bold text-3xl h-10 w-10" />
+        <SunIcon className="font-bold text-3xl h-10 w-10" />
       ) : (
         <MoonIcon className=" font-bold text-3xl h-10 w-10" />
       )}
