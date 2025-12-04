a7ba3114c5f2336cf4b45e032c47f277af607b4f
Laxman Rumba <rumbavai767@gmail.com>
2025-12-04 15:44:06 +0545

    refact/ added antialiased css

--- app/lib/helpers/axios.error.ts
+++ app/lib/helpers/axios.error.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 export function getAxiosErrorMessage(error: unknown): string {
   if (axios.isAxiosError(error)) {
     return (
-      error.response?.data?.message ?? error.message ?? "Something went wrong"
+      error.response?.data?.message ?? error.message ?? "Something went wrong!"
     );
   }
   if (error instanceof Error) {
     return error.message;
   }
-  return "Something went wrong";
+  return "Something went wrong!";
 }
