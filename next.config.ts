0f881fd363b8392b787e6925958c77584734d48d
Laxman Rumba <rumbavai767@gmail.com>
2025-12-04 12:01:08 +0545

    refact/tokenvalidation: added token validation

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
   },
 };
 
--- proxy.ts
+++ proxy.ts
@@ -1,18 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 
 const authPages = ["/login", "/register"];
 
 const protectedPages = ["/"];
-
+const JWT_SECRET = process.env.JWT_SECRET || "";
 export function proxy(req: NextRequest) {
-  const token = req.cookies.get("accessToken");
+  const token = req.cookies.get("accessToken")?.value;
   const { pathname } = req.nextUrl;
 
-  if (token && authPages.includes(pathname)) {
+  let isValidToken = false;
+
+  if (token) {
+    try {
+      jwt.verify(token, JWT_SECRET);
+      isValidToken = true;
+    } catch (err) {
+      isValidToken = false;
+    }
+  }
+
+  if (isValidToken && authPages.includes(pathname)) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  if (!token && protectedPages.includes(pathname)) {
+  if (!isValidToken && protectedPages.includes(pathname)) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
   return NextResponse.next();
